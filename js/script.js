16aaa36cf720516cdf59bd4951a867e1611f5e2c
tiko <tiko@gmail.com>
2019-07-24 18:02:17 +0400

    asaas

--- js/script.js
+++ js/script.js
@@ -1,4 +1,55 @@
 $(document).ready(function () {
+    $(".Login1").on("click",function () {
+        $("#login_form").removeClass("d-none");
+        $("#registration_form").addClass("d-none");
+    });
+    $(".Registration1").on("click",function () {
+        $("#registration_form").removeClass("d-none");
+        $("#login_form").addClass("d-none");
+    });
+
+
+    let inputBox = document.getElementById('first_inp');
+
+    inputBox.onkeyup = function () {
+
+        document.getElementById('first_text2').innerHTML = inputBox.value;
+    };
+    let inputBox1 = document.getElementById('last_input');
+    inputBox1.onkeyup = function () {
+        document.getElementById('last_name2').innerHTML = inputBox1.value;
+    }
+
+    let inputBox2 = document.getElementById('street_address');
+    inputBox2.onkeyup = function () {
+        document.getElementById('address_person').innerHTML = inputBox2.value;
+    }
+
+    let inputBox3 = document.getElementById('city_address');
+    inputBox3.onkeyup = function () {
+        document.getElementById('sity_sity').innerHTML = inputBox3.value;
+    }
+    let inputBox4 = document.getElementById('state_address');
+    inputBox4.onkeyup = function () {
+        document.getElementById('state').innerHTML = inputBox4.value;
+    }
+    let inputBox5 = document.getElementById('zip_code');
+    inputBox5.onkeyup = function () {
+        document.getElementById('zip_zip').innerHTML = inputBox5.value;
+    }
+    let inputBox6 = document.getElementById('email_address');
+    inputBox6.onkeyup = function () {
+        document.getElementById('email_email').innerHTML = inputBox6.value;
+    }
+    let inputBox7 = document.getElementById('phone');
+    inputBox7.onkeyup = function () {
+        document.getElementById('phone_phone').innerHTML = inputBox7.value;
+    }
+
+})
+
+
+$(document).ready(function () {
     window.globalIndex = 2;
     $('#owl1').owlCarousel({
         items: 3,
@@ -115,41 +166,7 @@ $(document).ready(function () {
     img();
 
 
-    var inputBox = document.getElementById('first_inp');
 
-    inputBox.onkeyup = function () {
-        document.getElementById('first_text2').innerHTML = inputBox.value;
-    };
-    var inputBox1 = document.getElementById('last_input');
-    inputBox1.onkeyup = function () {
-        document.getElementById('last_name2').innerHTML = inputBox1.value;
-    }
-
-    var inputBox2 = document.getElementById('street_address');
-    inputBox2.onkeyup = function () {
-        document.getElementById('address_person').innerHTML = inputBox2.value;
-    }
-
-    var inputBox3 = document.getElementById('city_address');
-    inputBox3.onkeyup = function () {
-        document.getElementById('sity_sity').innerHTML = inputBox3.value;
-    }
-    var inputBox4 = document.getElementById('state_address');
-    inputBox4.onkeyup = function () {
-        document.getElementById('state').innerHTML = inputBox4.value;
-    }
-    var inputBox5 = document.getElementById('zip_code');
-    inputBox5.onkeyup = function () {
-        document.getElementById('zip_zip').innerHTML = inputBox5.value;
-    }
-    var inputBox6 = document.getElementById('email_address');
-    inputBox6.onkeyup = function () {
-        document.getElementById('email_email').innerHTML = inputBox6.value;
-    }
-    var inputBox7 = document.getElementById('phone');
-    inputBox7.onkeyup = function () {
-        document.getElementById('phone_phone').innerHTML = inputBox7.value;
-    }
 });
 
 
